afc9c6df3428e5f719145870ce6ba3a0e9e6c760
DGolf1313 <103085521+DGolf1313@users.noreply.github.com>
2022-05-04 17:13:47 -0500

    second commit

---
+++ src/FirstTry.java
@@ -0,0 +1,28 @@
+
+public class FirstTry {
+
+	public static void main(String[] args) {
+	//create a variable to hold the remaining seats on an airplane 
+		int planeSeatsRemaining = 5;
+		
+		//create a variable to hold the cost of groceries at checkout
+		double groceryCost = 5.35;
+		
+		//create a variable to hold someones middle initial
+		char middleInitial = 'R';
+		
+		//create a varialbe to tell true if it's hot outside
+		boolean isHotOutside = true;
+		
+		//customers first name
+		String firstName = "Sabrina";
+		
+		//hold an address
+		String address = "1234 Main St";
+		
+		//print all variables. 
+		System.out.println(planeSeatsRemaining + ", " + groceryCost + ", " + middleInitial + ", " + isHotOutside + ", " + firstName + ", " + address);
+		
+	}
+
+}
